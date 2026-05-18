88ce93ee49c0443c6cfdeec2db6c2a921a050180
RohitKumar <rohitgowthwal06@gmail.com>
2026-05-18 11:41:05 +0530

    Day-4 Added largest element finder with index tracking

--- Day-4/Largest.java
+++ Day-4/Largest.java
@@ -10,12 +10,15 @@ public class Largest {
             arr[i] = sc.nextInt();
         }
         int max = arr[0];
+        int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
+                count = i;
             } 
         }
-        System.out.println("The largest element in the array is : "+ max );
+        System.out.println("The largest element in the array is : "+ max + ", at index no. : " + count);
+        
         sc.close();
     }    
 }
