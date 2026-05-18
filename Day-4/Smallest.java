9e032f7ad18e7a30e307ca7039e539be37771c2a
RohitKumar <rohitgowthwal06@gmail.com>
2026-05-18 11:52:03 +0530

    Day-4 Added smallest element finder with index tracking

--- Day-4/Smallest.java
+++ Day-4/Smallest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Largest {
+public class Smallest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int [] arr = new int[5];
@@ -9,24 +9,15 @@ public class Largest {
             System.out.print("Enter element " + (i + 1) + " : ");
             arr[i] = sc.nextInt();
         }
-        int max = arr[0];
+        int min = arr[0];
         int count = 0;
-         int min = arr[0];
-        int counts = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-                count = i;
-            } 
             if (arr[i] < min) {
                 min = arr[i];
-                counts = i;
-            }
+                count = i;
+            } 
         }
-       
-        
-        System.out.println("The smallest element in the array is : "+ min + ", at index no. : " + counts);
-        System.out.println("The largest element in the array is : "+ max + ", at index no. : " + count);
+        System.out.println("The smallest element in the array is : "+ min + ", at index no. : " + count);
         
         sc.close();
     }    
