a4f2d510f019c3b6d39b7eb1953420dbe86c6b03
RohitKumar <rohitgowthwal06@gmail.com>
2026-05-18 11:33:00 +0530

    Day-4 Added a program to find the largest element in the array

---
+++ Day-4/Largest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] arr = new int[5];
+        System.out.println("Enter the elements of an array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter element " + (i + 1) + " : ");
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            } 
+        }
+        System.out.println("The largest element in the array is : "+ max );
+        sc.close();
+    }    
+}
