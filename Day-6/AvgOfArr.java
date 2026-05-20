6f79d1b2f44d9d092fa7b3ccc350e61b727ee9bb
RohitKumar <rohitgowthwal06@gmail.com>
2026-05-20 11:02:46 +0530

    Day-6: Added a program which gives Average of an array

---
+++ Day-6/AvgOfArr.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AvgOfArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements of an Array : ");
+        int n = sc.nextInt();
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter element " + (i + 1)+ " : ");
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("The original array is : "+ Arrays.toString(arr));
+        int avg = sum/n;
+        System.out.println("The avg of the array is : " + avg);
+        sc.close();
+    }
+}
