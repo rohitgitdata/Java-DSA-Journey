41f05a5b643fb6f1e993eb28a6d783db9ca99e12
RohitKumar <rohitgowthwal06@gmail.com>
2026-05-22 12:17:46 +0530

    Updated Palindrome Number program and maked it more readable

--- Week-1/Day-3/PalindromeN.java
+++ Week-1/Day-3/PalindromeN.java
@@ -13,6 +13,9 @@ public class PalindromeN{
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("=========Program Strated==========");
+        boolean flag = true;
+        while (flag) {
         System.out.print("Enter the number : " );
         int num = sc.nextInt();
         
@@ -21,6 +24,13 @@ public class PalindromeN{
         } else {
             System.out.println("Entered number is not palindrome");
         }
+        System.out.print("You want to continue? (Y/N)");
+        String ch = sc.next();
+        if (ch.equalsIgnoreCase("N")) {
+            flag = false;
+        }
+    }
+        System.out.print("=========Program Completed==========");
         sc.close();
     }
 }
