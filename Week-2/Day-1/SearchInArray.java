65ea6b3f67437698e027044fafe148107a708a54
RohitKumar <rohitgowthwal06@gmail.com>
2026-05-23 11:31:49 +0530

    Week-2 Day-1 added program which finds elements in the array using linear search algorithm

---
+++ Week-2/Day-1/SearchInArray.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class SearchInArray {
+    static int element(int[] arr, int target){
+        for (int i = 0; i< arr.length; i++) {
+            if (arr[i] == target) {
+                return i; 
+            }
+        }
+        return -1;
+    }    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter the " + (i + 1) + " element : ");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Our array is : " + Arrays.toString(arr));
+        System.out.print("Enter the target : " );
+        int target = sc.nextInt();
+        int res = element(arr, target);
+        if (res != -1) {
+            System.out.println("Element found at index number " + res);
+        } else {
+            System.out.println("Element is not found in the array");
+        }
+        sc.close();
+    }
+}
