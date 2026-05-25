484c681486d9096c6d2273f3938f1863656e8f6b
RohitKumar <rohitgowthwal06@gmail.com>
2026-05-25 10:17:53 +0530

    Week-2 Day-3 Updated a program for searching element in asc/desc. sorted array using Order Agnostic Binary Search

--- Week-2/Day-3/OrderAgnosticBS.java
+++ Week-2/Day-3/OrderAgnosticBS.java
@@ -4,9 +4,28 @@ public class OrderAgnosticBS {
     static int searchElement(int[] arr, int target){
         int start = 0;
         int end = arr.length - 1;
+        boolean isAsc = arr[start] < arr[end];
         while (start <= end) {
-        int mid = start + (end - start)/2;
-        if (arr[start] < arr[end]) {
+            int mid = start + (end - start)/2;
+            if (arr[mid] == target) {
+                return mid;
+            }     
+            if (isAsc) {
+                if (arr[mid] < target) {
+                start = mid + 1;
+                } else if(arr[mid] > target){
+                end = mid - 1;
+                }
+            } else {
+                if (arr[mid] < target) {
+                end = mid - 1;
+                } else if(arr[mid] > target){
+                start = mid + 1;
+                }
+            }
+
+
+       /*  if (arr[start] < arr[end]) {
             //Ascending
             if (arr[mid] < target) {
                 start = mid + 1;
@@ -23,9 +42,10 @@ public class OrderAgnosticBS {
                 start = mid + 1;
             }else{
                 return mid;
-            }
-        }
+            } 
+        } */
     }
+    
         return -1;
     }
     public static void main(String[] args) {
