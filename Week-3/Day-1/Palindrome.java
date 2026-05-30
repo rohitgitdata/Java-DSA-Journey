d4cef708712cb743aa8cf686274dd71e8b361be9
RohitKumar <rohitgowthwal06@gmail.com>
2026-05-30 11:34:51 +0530

    Updated  a java Program to find that the String After Ignoring case and other unwanted digits or letter is Palindrome or not

--- Week-3/Day-1/Palindrome.java
+++ Week-3/Day-1/Palindrome.java
@@ -1,16 +1,32 @@
 public class Palindrome {
-    static boolean check(String name){
+    static boolean check(String s){
         int start = 0;
-        int end = name.length() - 1;
-        while (start <= end) {
-            if (name.charAt(start) != name.charAt(end)) {
+        int end = s.length() - 1;
+
+        while(start <= end){
+
+            while(start < end && !Character.isLetterOrDigit(s.charAt(start))){
+                start++;
+            }
+
+            while(start < end && !Character.isLetterOrDigit(s.charAt(end))){
+                end--;
+            }
+
+            char left = Character.toLowerCase(s.charAt(start));
+            char right = Character.toLowerCase(s.charAt(end));
+
+            if(left != right){
                 return false;
-            } 
+            }
+
             start++;
             end--;
         }
+
         return true;
-    }   
+    }
+  
     public static void main(String[] args) {
         String name = "Naman";
         boolean ans = check(name.toLowerCase());
