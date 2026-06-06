3807466ca61b90ca30c5674362c506c5f1762137
Rohit Kumar <rohitgowthwal06@gmail.com>
2026-06-06 11:14:43 +0530

    Revised a program which gives the pattern of inverted Right angled triangle

---
+++ Week-3/Day-6/RevisedPattern1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RevisedPattern1 {
+    static void pattern(int rows, int cols){
+        if (rows == 0) {
+            return;
+        }
+        if (cols < rows) {
+            System.out.print("*");
+            pattern(rows, cols + 1);
+        }else{
+            System.out.println();
+            pattern(rows - 1, 0);
+        }
+    }    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows : ");
+        int rows = sc.nextInt();
+        pattern(rows, 0);
+        sc.close();
+    }
+}
