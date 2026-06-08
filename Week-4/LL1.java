5c3b8aa51ff6f0795b9f120a96bed743c130cb56
Rohit Kumar <rohitgowthwal06@gmail.com>
2026-06-08 12:02:37 +0530

    Added a porgram after learning the concept of linked list

---
+++ Week-4/LL1.java
@@ -0,0 +1,54 @@
+public class LL1 {
+    //Starting node
+    private Node head;
+    //Last node
+    private Node tail;
+    //Total node
+    private int size;
+    
+    public LL1() {
+        size = 0;
+    }
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+        if (tail == null) {
+            tail = head;
+        }
+        size++;
+    }
+    public void insertLast(int val){
+        if (head == null) {
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = tail.next;
+        size++;
+    }
+    public void display(){
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+    private class Node{
+        private int value;
+        private Node next;
+        public Node(int value){
+            this.value = value;
+        }//Constructor Close
+    }//Node Class close
+    public static void main(String[] args) {
+        LL1 list = new LL1();
+        list.insertFirst(10);
+        list.insertFirst(20);
+        list.insertFirst(30);
+        list.insertLast(40);
+        list.display();
+    }
+}//LL1 close
