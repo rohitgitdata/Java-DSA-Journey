8a2d2a21f93224ce797e06180434fe85f4ca5734
Rohit Kumar <rohitgowthwal06@gmail.com>
2026-06-09 12:49:23 +0530

    Added a program which give the reverse of the single linked list

---
+++ Week-4/Day-2/SLL.java
@@ -0,0 +1,72 @@
+public class SLL {
+
+    private Node head;
+    private int size;
+
+    public SLL() {
+        this.size = 0;
+    }
+
+    public void insertFirst(int value) {
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
+        size++;
+    }
+
+    public void display() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("END");
+    }
+
+    public void reverse() {
+
+        Node prev = null;
+        Node curr = head;
+
+        while (curr != null) {
+
+            Node next = curr.next;
+
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+    }
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        SLL list = new SLL();
+
+        list.insertFirst(5);
+        list.insertFirst(4);
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(1);
+
+        System.out.println("Original List:");
+        list.display();
+
+        list.reverse();
+
+        System.out.println("Reversed List:");
+        list.display();
+    }
+}
