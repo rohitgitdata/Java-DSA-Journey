1281dc9808e8c05c9db8d75e904e24eea130b16b
Rohit Kumar <rohitgowthwal06@gmail.com>
2026-06-10 11:15:50 +0530

    Updated a program for delete the nodes of the circular linked list

--- Week-4/Day-3/CLL.java
+++ Week-4/Day-3/CLL.java
@@ -13,10 +13,34 @@ public class CLL {
         if (head == null) {
             head = node;
             tail = node;
+            node.next = head;
+            return;
         }
         tail.next = node;
         node.next = head;
         tail = node;
+    }
+    public int delete(int val){
+        if (head == null) {
+            return 0;
+        }
+        if (head.value == val) {
+            tail.next = head.next;
+            head = head.next;
+            return val;
+        }
+        Node temp = head;
+        do {
+            if (temp.next.value == val) {
+                     if (temp.next == tail) {
+                        tail = temp;
+                }
+                temp.next = temp.next.next;
+                return val;
+            }
+            temp = temp.next;
+        } while (temp != head);
+        return -1;
     }
     public void display(){
         Node temp = head;
@@ -44,6 +68,8 @@ public class CLL {
         list.insert(2);
         list.insert(4);
         list.insert(7);
+        list.display();
+        list.delete(4);
         list.display();
     }
 } // CLL class ends
