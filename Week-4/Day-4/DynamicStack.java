794de3ea1d2e01570334ad383c0ef1919008533e
Rohit Kumar <rohitgowthwal06@gmail.com>
2026-06-12 11:04:54 +0530

    Added a program of stack in which there is no limit of size use it dynamically

---
+++ Week-4/Day-4/DynamicStack.java
@@ -0,0 +1,73 @@
+public class DynamicStack {
+        protected int[] data;
+        private static final int DEFAULT_SIZE = 10;
+        protected int ptr = -1;
+        
+        public DynamicStack(){
+            this(DEFAULT_SIZE);
+        }
+
+        public DynamicStack(int size) {
+            this.data = new int[size];
+        }
+        
+        public boolean isEmpty(){
+            return ptr == -1;
+        }
+        
+        public boolean push(int item){
+           if (ptr == data.length - 1) {
+            int[] temp = new int[data.length * 2];
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[i];
+            }
+            data = temp;
+           }
+            ptr++;
+            data[ptr] = item;
+            return true;
+        }
+        public int pop() throws Exception{
+            if (isEmpty()) {
+                throw new Exception("Stack is Empty");
+            }
+            int removed = data[ptr];
+            ptr--;
+            return removed;
+        }
+        public int peek() throws Exception{
+            if (isEmpty()) {
+                System.out.println("stack is empty");
+            }
+            int top = data[ptr];
+            return top;
+        }
+        public static void main(String[] args)throws Exception {
+            DynamicStack stack = new DynamicStack(10);
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60);
+        stack.push(70);
+        stack.push(80);
+        stack.push(90);
+        stack.push(100);
+        stack.push(10);
+            System.err.println("The last element of the stack is : " + stack.peek());
+
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println("| " + stack.pop() + " |");
+        System.out.println(("| __ |"));            
+        }
+}
