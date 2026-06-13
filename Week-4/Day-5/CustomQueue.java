bac2b07e980e4e5cfc7adac6902ff98afe1d3fe0
Rohit Kumar <rohitgowthwal06@gmail.com>
2026-06-13 11:15:30 +0530

    Added a program of custom queue including some major mehtod of queue made by me

---
+++ Week-4/Day-5/CustomQueue.java
@@ -0,0 +1,99 @@
+public class CustomQueue {
+    private int[] data;
+    private static final int DEFAULT_SIZE = 10;
+        protected int end = 0;
+        private int front = 0;
+        private int size = 0;
+        
+        public CustomQueue(){
+            this(DEFAULT_SIZE);
+        }
+
+        public CustomQueue(int size) {
+            this.data = new int[size];
+        }
+        
+        public boolean isFull(){
+            return size == data.length;
+        }
+        public boolean isEmpty(){
+            return size == 0;
+        }    
+        public boolean insert(int item) throws Exception{
+            if (isFull()) {
+                throw new Exception("Queue is OVERFLOW");
+            }
+            data[end++] = item;
+            size++;
+            return true;
+        }
+        public int remove()throws Exception{
+            if (isEmpty()) {
+                throw new Exception("Queue is EMPTY");
+            }
+            int removed = data[front];
+            for (int i = 0; i < size - 1; i++) {
+                data[i] = data[i + 1];
+            }
+            end--;
+            size--;
+            return removed;
+        }
+        public int peek(){
+            if (isEmpty()) {
+                return -1;
+            }
+            return data[front];
+        }
+        public void display(){
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return;
+            }
+            for (int i = 0; i < size; i++) {
+                System.out.print(data[i] + " <- ");
+            }
+            System.out.println("END");
+        }
+        public static void main(String[] args) throws Exception{
+            CustomQueue queue = new CustomQueue(10);
+            System.out.println();
+        try {
+            queue.insert(10);
+            queue.insert(20);
+            queue.insert(30);
+            queue.insert(40);
+            queue.insert(50);
+            queue.insert(60);
+            queue.insert(70);
+            queue.insert(80);
+            queue.insert(90);
+            queue.insert(100);
+            queue.insert(10);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println();
+           try {
+            System.err.println("The first element of the queue is : " + queue.peek());
+           } catch (Exception e) {
+            System.out.println(e.getMessage());
+           }
+           System.out.println();
+        try {
+            System.out.println("| " + queue.remove() + " |");
+        System.out.println("| " + queue.remove() + " |");
+        System.out.println("| " + queue.remove() + " |");
+        System.out.println("| " + queue.remove() + " |");
+        System.out.println("| " + queue.remove() + " |");
+        System.out.println("| " + queue.remove() + " |");
+        System.out.println("| " + queue.remove() + " |");
+        System.out.println("| " + queue.remove() + " |");
+        System.out.println("| " + queue.remove() + " |");
+        System.out.println("| " + queue.remove() + " |");
+        System.out.println(("| __ |"));            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
