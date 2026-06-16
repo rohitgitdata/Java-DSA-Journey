86d446edf74159b8b7830466daccd91b85e76d1f
Rohit Kumar <rohitgowthwal06@gmail.com>
2026-06-16 17:00:24 +0530

    Added a porgram which gives the difference of the sum and product of the digit of an Integer

---
+++ Week-4/Day-6/DiffOfSumAndProd.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class DiffOfSumAndProd {
+    int sum  = 0;
+    int prod = 1;
+    public int subtractProductAndSum(int n) {
+        
+        while(n > 0){
+        int rem = n % 10;
+        sum += rem;
+        prod *= rem;
+        n = n/10;
+        }
+        int diff = prod - sum;
+        return diff;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        DiffOfSumAndProd obj = new DiffOfSumAndProd();
+        int ans = obj.subtractProductAndSum(n);
+        System.out.println("So the difference of the sum and the product of the digits is : "+ ans);
+        sc.close();
+
+    }    
+}
